77b8710a45fb14b0b32f8516cad0b1d6971c704f
zhangquanwei <zhangquanwei@yunzhangfang.com>
2018-08-27 19:14:45 +0800

    验证当有事务的时候 读取事务中的数据库链接，不切换数据源查从库

--- src/main/java/com/jova/wrid/controller/TestController.java
+++ src/main/java/com/jova/wrid/controller/TestController.java
@@ -1,37 +1,32 @@
 package com.jova.wrid.controller;
 
-import com.jova.wrid.dao.UserDao;
+
 import com.jova.wrid.entity.TAaaUsers;
-import com.jova.wrid.mapper.TAaaUsersMapper;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Date;
 
 @RestController
 public class TestController {
 
     @Autowired
-    private UserDao userDao;
+    private com.jova.wrid.service.UserService userService;
 
-    @RequestMapping("/test")
+    @RequestMapping("/updateAndSelect")
     public  String getUser(@RequestParam(name = "id") Long usrId){
-        TAaaUsers users = new TAaaUsers();
-        users.setId(usrId);
-        users.seteMail("xxxx_@yzf.com_"+System.currentTimeMillis());
-        users.setCreateTime(new Date());
-        userDao.updateByPrimaryKeySelective(users);
-        return  userDao.selectByPrimaryKey(usrId).toString();
+
+      return userService.updateAndSelect(usrId);
     }
 
     @RequestMapping("/delete/{id}")
     public  String delete(@PathVariable("id") Long id){
 
 
-          userDao.deleteByPrimaryKey(id);
+        userService.deleteByPrimaryKey(id);
         return "success";
     }
 
@@ -42,7 +37,7 @@ public class TestController {
         users.setPhoneNo("1111");
         users.setPassword("321654");
         users.setName("xiaoxiao_"+userName);
-        userDao.insertSelective(users);
+        userService.insertSelective(users);
         return "success";
     }
 
---
+++ src/main/java/com/jova/wrid/service/impl/UserServiceImpl.java
@@ -0,0 +1,38 @@
+package com.jova.wrid.service.impl;
+
+import com.jova.wrid.dao.UserDao;
+import com.jova.wrid.entity.TAaaUsers;
+import com.jova.wrid.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Date;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserDao userDao;
+
+    @Transactional
+    @Override
+    public String updateAndSelect(Long usrId) {
+        TAaaUsers users = new TAaaUsers();
+        users.setId(usrId);
+        users.seteMail("xxxx_@yzf.com_"+System.currentTimeMillis());
+        users.setCreateTime(new Date());
+        userDao.updateByPrimaryKeySelective(users);
+        return userDao.selectByPrimaryKey(usrId).toString();
+    }
+
+    @Override
+    public int deleteByPrimaryKey(Long id) {
+        return userDao.deleteByPrimaryKey(id);
+    }
+
+    @Override
+    public int insertSelective(TAaaUsers users) {
+        return userDao.insertSelective(users);
+    }
+}
